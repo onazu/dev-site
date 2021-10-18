cb5c498692d8bb1e90141cb77cc39c62a1dd4391
onazu <92691447+onazu@users.noreply.github.com>
2021-10-18 16:20:37 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+session_start();
+
+    include("connection.php");
+    include("functions.php");
+
+     $user_data = check_login($con);
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Ma machine</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <script type="text/javascript" src="app.js"></script>
+    </head>
+    <body>
+        <div id="header">
+            <button class="menubutton">☰</button>
+            <button class="menubutton">Ma machine</button>
+            <button class="menubutton">Machine de mon équipe</button>
+            <button onclick="window.location.href='logout.php'" class="menubutton">Déconnexion</button>
+            <button class="menubutton">?</button>
+        </div>
+        <div class="window">
+            <h1>NomProjet x Bureau</h1>
+        </div>
+        <div class="window">
+            Bonjour <?= $user_data['user_name'] ?><br><br><br>
+            <button class="blockbutton">Sell</button>
+        </div>
+    </body>
+</html>
